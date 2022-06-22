5c310ea4c2f8c888297c0fa615170232408a5e48
sebastiandres <sebastiandres@gmail.com>
2022-06-21 21:02:36 -0400

    final touches

--- private/home.py
+++ private/home.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import streamlit_book as stb
 
-st.title("Landing")
+st.title("Private Home")
 
 st.markdown("""
 This is where you should put your **private** landing page.
-Just edit the file `private/landing.py`.
+Just edit the file `private/home.py`.
 
 Maybe you put some considerations on the reason to have a private space.
 """)
